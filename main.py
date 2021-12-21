fa31eb3b48465131c8b39a05f65e319360f95afe
PyEddy <55319774+PyEddy@users.noreply.github.com>
2021-12-21 13:28:18 +0000

    Made the code look better

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import netifaces as ni
 
 
 def getIP():
-    ni.ifaddresses('eth0')
-    IP = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
-    return IP
+    """Grabs the IP from the eth0 interface"""
+    try:
+        ni.ifaddresses('eth0')
+        IP = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+        return IP
+    except:
+        print("Error: Couldn't find eth0 interface")
+        print("Make sure to read the README.md before using this tool")
+        exit()
+
 
 
 def readAuth(ip_address):
+    """Reads authconfig and writes the grabbed ip to external_ip"""
     with open("authconfig.ini", "r") as f:
         lines = f.readlines()
         external_ip = lines[7]
@@ -16,9 +24,11 @@ def readAuth(ip_address):
     with open("authconfig.ini", "w") as f:
         lines[7] = "external_ip=" + ip_address + "\n"
         f.writelines(lines)
+        f.close()
 
 
 def readChat(ip_address):
+    """Reads chatconfig and writes the grabbed ip to external_ip"""
     with open("chatconfig.ini", "r") as f:
         lines = f.readlines()
         external_ip = lines[7]
@@ -26,9 +36,11 @@ def readChat(ip_address):
     with open("chatconfig.ini", "w") as f:
         lines[7] = "external_ip=" + ip_address + "\n"
         f.writelines(lines)
+        f.close()
 
 
 def readMaster(ip_address):
+    """Reads masterconfig and writes the grabbed ip to external_ip"""
     with open("masterconfig.ini", "r") as f:
         lines = f.readlines()
         external_ip = lines[7]
@@ -36,6 +48,7 @@ def readMaster(ip_address):
     with open("masterconfig.ini", "w") as f:
         lines[7] = "external_ip=" + ip_address + "\n"
         f.writelines(lines)
+        f.close()
 
 
 external_ip = getIP()
